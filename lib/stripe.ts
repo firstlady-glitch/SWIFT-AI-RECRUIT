829f694f694b73cfca5c34ade9350685e55889bb
kingjethro999 <jethrojerrybj@gmail.com>
2026-04-05 17:08:06 +0100

    added pnpm

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -9,7 +9,7 @@ const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 
 export const stripe = stripeSecretKey
     ? new Stripe(stripeSecretKey, {
-        apiVersion: '2025-12-15.clover',
+        apiVersion: '2026-02-25.clover',
         typescript: true,
     })
     : null;
